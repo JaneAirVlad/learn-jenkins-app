741550064b0ddee837cbd844fea6e4e1bddaf820
JaneAirVlad <124638379+JaneAirVlad@users.noreply.github.com>
2025-03-05 15:36:55 +0300

    Add files via upload

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+// src/test/java/MainTest.java
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testExample() {
+        assertEquals(1, 1);
+    }
+}
